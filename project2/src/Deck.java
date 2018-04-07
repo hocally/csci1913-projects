107235b7d923a233a52ae3c4bd0010f5fb871cbb
hocally <henry.ocallaghan@gmail.com>
2018-04-07 15:55:09 -0500

    Fixed off by one error in deck dealing

--- project2/src/Deck.java
+++ project2/src/Deck.java
@@ -7,7 +7,7 @@ public class Deck {
 
     public Deck() {
         count = 0;
-        deck = new Card[53];
+        deck = new Card[52];
         for(int i = 0; i < deck.length; i++) {
             deck[i] = new Card(Math.abs(rand.nextInt()) % 13 + 1);
             count++;
@@ -15,7 +15,7 @@ public class Deck {
     }
 
     public Card deal() {
-        if(count > 1) {
+        if(count > 0) {
             return deck[(count--) - 1];
         } else {
             throw new IllegalStateException("No cards left in deck");
@@ -23,7 +23,7 @@ public class Deck {
     }
 
     public void shuffle() {
-        if(count < 51) {
+        if(count <= 51) {
             throw new IllegalStateException("Cannot shuffle after dealing has started");
         } else {
             for(int i = deck.length - 1; i > 0; i--) {
